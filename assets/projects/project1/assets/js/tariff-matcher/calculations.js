828cb542652f8c8fb493cc5cfc3966413aa75af5
bradley.velle <bradley.velle@student.howest.be>
2024-12-12 16:59:51 +0100

    Update Project 1 to be final version

--- assets/projects/project1/assets/js/tariff-matcher/calculations.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-function calculateMonthlyPrice(pricePerkWh, estimatedkWh) {
-    return formatNumber((pricePerkWh * estimatedkWh) / 12, 2);
-}
-
-function calculateVAT(price, vatPercentage) {
-    return formatNumber((price * vatPercentage) / 100, 2);
-}
